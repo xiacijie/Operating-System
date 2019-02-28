c2e20683d5fd80efd20b14682bdc8a7dbf7f770b
xiacijie <cijie@ualberta.ca>
2019-02-27 23:50:07 -0700

    IPC shared memory

--- Process/fork.c
+++ Process/fork.c
@@ -4,14 +4,18 @@
 
 int main(){
     pid_t pid;
+    // printf("1. ----- PID: %d\n",pid);
     // fork a child process
     pid = fork();
+    pid = fork();
+    // printf("2. ----- PID: %d\n",pid);
     if (pid < 0){ // error occured
         fprintf(stderr,"Fork Failed");
         return 1;
     }
 
     else if (pid == 0){ // child process
+        printf("Child exec\n");
         execlp("/bin/ls","ls",NULL);
     }
     else{ //parent process
---
+++ Process/fork_test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+ int main(int argc, char *argv[])
+ {
+
+    
+    fork();
+    
+    fork();
+
+    puts("hi");
+    
+
+    
+
+    return 0;
+ }
